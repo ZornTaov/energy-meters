e36c91d51b5b0c3f4279da4b8b51eedcd9dd4aec
ZornTaov <zeke_kjell2@yahoo.com>
2020-10-08 22:08:39 -0700

    revert deleting the dynamic baked model as per suggestion

--- src/main/java/com/vladmarica/energymeters/client/ClientProxy.java
+++ src/main/java/com/vladmarica/energymeters/client/ClientProxy.java
@@ -1,17 +1,27 @@
 package com.vladmarica.energymeters.client;
 
+import com.google.common.collect.Iterators;
 import com.vladmarica.energymeters.CommonProxy;
 import com.vladmarica.energymeters.EnergyMetersMod;
+import com.vladmarica.energymeters.Registration;
 import com.vladmarica.energymeters.block.BlockEnergyMeter;
 import com.vladmarica.energymeters.block.Blocks;
 import com.vladmarica.energymeters.client.gui.EnergyMeterScreen;
+import com.vladmarica.energymeters.client.model.EnergyMeterBakedModel;
+import com.vladmarica.energymeters.client.model.TexturedQuadCache;
 import com.vladmarica.energymeters.tile.TileEntityEnergyMeterBase;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Random;
 
 import com.vladmarica.energymeters.tile.TileEntityTypes;
+import net.minecraft.block.BlockState;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.BlockModelShapes;
 import net.minecraft.client.renderer.model.BakedQuad;
+import net.minecraft.client.renderer.model.IBakedModel;
+import net.minecraft.client.renderer.model.ModelResourceLocation;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.tileentity.TileEntity;
@@ -23,12 +33,15 @@ import net.minecraft.world.World;
 import net.minecraftforge.client.event.ModelBakeEvent;
 import net.minecraftforge.client.event.ModelRegistryEvent;
 import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.client.model.data.EmptyModelData;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.client.registry.ClientRegistry;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 public class ClientProxy extends CommonProxy {
+
+  private static final Random tempRandom = new Random();
 
   @Override
   public void init(FMLCommonSetupEvent event) {
@@ -55,7 +68,40 @@ public class ClientProxy extends CommonProxy {
 
   @SubscribeEvent
   public static void registerSpecialModels(final ModelRegistryEvent registryEvent) {
-    //ModelLoader.addSpecialModel(new ResourceLocation(EnergyMetersMod.MODID, "block/meter"));
+    ModelLoader.addSpecialModel(new ResourceLocation(EnergyMetersMod.MODID, "block/meter"));
+  }
+  @SubscribeEvent
+  public void onModelBaking(ModelBakeEvent event) {
+    Map<Direction, BakedQuad> cubeQuadMap = new HashMap<>();
+    Map<ResourceLocation, TextureAtlasSprite> spriteMap = new HashMap<>();
+
+    overrideModel(event, Blocks.ENERGY_METER_FE, cubeQuadMap, spriteMap);
+
+    // Store cube quads and textures in the cache
+    TexturedQuadCache.INSTANCE.setCubeQuadMap(cubeQuadMap);
+    TexturedQuadCache.INSTANCE.setTextureMap(spriteMap);
   }
 
+  private static void overrideModel(ModelBakeEvent event, BlockEnergyMeter block, Map<Direction, BakedQuad> cubeQuadMap, Map<ResourceLocation, TextureAtlasSprite> spriteMap) {
+    for (BlockState state : block.getStateContainer().getValidStates()) {
+      ModelResourceLocation modelLocation = BlockModelShapes.getModelLocation(state);
+      IBakedModel originalModel = event.getModelRegistry().get(modelLocation);
+
+      // Extract textured quads from default model
+      for (Direction side : Direction.values()) {
+        List<BakedQuad> quads = originalModel.getQuads(state, side, tempRandom, EmptyModelData.INSTANCE);
+        BakedQuad quad = Iterators.getOnlyElement(quads.iterator());
+
+        ResourceLocation spriteLocation = quad.getSprite().getName();
+        if (!spriteLocation.getPath().equals("missingno") && !spriteMap.containsKey(spriteLocation)) {
+          spriteMap.put(spriteLocation, quad.getSprite());
+          EnergyMetersMod.LOGGER.debug("Side {} has texture {}", side.getName2(), quad.getSprite());
+        }
+
+        cubeQuadMap.put(side, quad);
+      }
+
+      event.getModelRegistry().put(modelLocation, new EnergyMeterBakedModel(originalModel));
+    }
+  }
 }
